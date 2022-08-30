25388d037ab90d4f0c9a6944b43ffc081d8eed8f
sohaibas <sohaibsiddiqui42@gmail.com>
2022-08-30 11:12:55 +0200

    First version with skeleton code

---
+++ src/ukeoppgaver1.java
@@ -0,0 +1,19 @@
+public class ukeoppgaver1 {
+    public static void main(String[]args) {
+        System.out.println("Hello World");
+
+        int[] a = {8, 4, 17, 10, 6, 20, 1, 11, 15, 3, 18, 9, 2, 7, 19};
+
+        int min_index = min(a);
+
+        System.out.println(min_index);
+    }
+
+
+    static int min(int[] a) {
+        int min_value = 0;
+        int min_index = 0;
+
+        return min_index;
+    }
+}
